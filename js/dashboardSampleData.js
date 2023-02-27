11ea8a1dead495adfb3e7a1df9c531d22f4ca527
nc8005 <nc8005@g.rit.edu>
2023-02-26 20:22:15 -0500

    Dashboard Iteration 1.1

--- js/dashboardSampleData.js
+++ js/dashboardSampleData.js
@@ -1,33 +1,95 @@
-var sampleDataString = "" +
+let sampleDataStringRecentUsers = "" +
     "<thead>" +
     "<tr>" +
     "<th>Student ID</th>" +
-    "<th>Student First Name</th>" +
-    "<th>Student Middle Initial</th>" +
-    "<th>Student Last Name</th>" +
-    "<th>Student Username</th>" +
-    "<th>Student School</th>" +
+    "<th>First Name</th>" +
+    "<th>Middle Initial</th>" +
+    "<th>Last Name</th>" +
+    "<th>Username</th>" +
+    "<th>School</th>" +
     "</tr>" +
-    "</thead>\n" +
+    "</thead>" +
 
     "<tr>" +
-    "<th>1</th>" +
-    "<th>Ryan</th>" +
-    "<th>N</th>" +
-    "<th>Vay</th>" +
-    "<th>rxv7131</th>" +
-    "<th>CIS</th>" +
-    "</tr>\n" +
+    "<td>1</td>" +
+    "<td>Ryan</td>" +
+    "<td>N</td>" +
+    "<td>Vay</td>" +
+    "<td>rxv7131</td>" +
+    "<td>CIS</td>" +
+    "</tr>" +
 
     "<tr>" +
-    "<th>2</th>" +
-    "<th>Evan</th>" +
-    "<th>M</th>" +
-    "<th>Vay</th>" +
-    "<th>EvanUsername123</th>" +
-    "<th>CS</th>" +
-    "</tr>\n";
+    "<td>2</td>" +
+    "<td>Evan</td>" +
+    "<td>M</td>" +
+    "<td>Vay</td>" +
+    "<td>EvanUsername123</td>" +
+    "<td>CSEC</td>" +
+    "</tr>" +
 
+    "<tr>" +
+    "<td>3</td>" +
+    "<td>Boris</td>" +
+    "<td>G</td>" +
+    "<td>McDonald</td>" +
+    "<td>BGMD</td>" +
+    "<td>CSEC</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<td>5</td>" +
+    "<td>Johnny</td>" +
+    "<td>B</td>" +
+    "<td>Goode</td>" +
+    "<td>JBGoode23</td>" +
+    "<td>iSchool</td>" +
+    "</tr>"
+;
+
+let sampleDataStringRecentLogs = "" +
+    "<thead>" +
+    "<tr>" +
+    "<th>Log ID</th>" +
+    "<th>Log Time Created</th>" +
+    "<th>Log Time Edited</th>" +
+    "<th>Login Attempt ID</th>" +
+    "<th>Student ID</th>" +
+    "</tr>" +
+    "</thead>" +
+
+    "<tr>" +
+    "<td>1</td>" +
+    "<td>2023-01-23 12:45:56</td>" +
+    "<td>2023-01-23 12:45:56</td>" +
+    "<td>1</td>" +
+    "<td>rxv7131</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<td>2</td>" +
+    "<td>2023-01-23 12:46:19</td>" +
+    "<td>2023-01-23 12:46:19</td>" +
+    "<td>2</td>" +
+    "<td>rxv7131</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<td>3</td>" +
+    "<td>2023-01-25 08:23:42</td>" +
+    "<td>2023-01-25 08:23:42</td>" +
+    "<td>4</td>" +
+    "<td>EvanUsername</td>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<td>4</td>" +
+    "<td>2023-01-28 11:19:07</td>" +
+    "<td>2023-01-28 11:19:07</td>" +
+    "<td>5</td>" +
+    "<td>BGMD</td>" +
+    "</tr>"
+;
 
 
 function getSampleData(){
@@ -36,5 +98,6 @@ function getSampleData(){
 
 
 window.onload = function(){
-    $("#studentTable").append( sampleDataString );
+    $("#studentDashboardTable").append( sampleDataStringRecentUsers );
+    $("#logDashboardTable").append( sampleDataStringRecentLogs );
 };
